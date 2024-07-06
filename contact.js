2cca311db78b2cca0ce56f01d492921692b74adc
swathy <swathyswathy24610@gmail.com>
2024-07-06 10:51:03 +0530

    nova-1

--- contact.js
+++ contact.js
@@ -1,7 +1,5 @@
-// script.js
 
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for your message!');
-    // Here you can add additional form submission logic (e.g., send data to a server)
 });
